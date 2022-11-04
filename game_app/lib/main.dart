b4bd3451106e53554e9ef13870cdc90d2c21b1a3
Tamne2022002 <0306201074@caothang.edu.vn>
2022-11-04 22:31:46 +0700

    Update Screen Login

--- game_app/lib/main.dart
+++ game_app/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'screen_maingame/screen_maingame.dart';
+import 'screen_sign_in/form_sign_in.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,8 +11,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: ScreenMainGame(),
+    return const MaterialApp(
+      home: Form_SignIn(),
     );
   }
 }
