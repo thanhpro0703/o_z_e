d344095641152fa3e752fbdb428083ac9767f70e
ngocthanh0703 <0306201078@caothang.edu.vn>
2022-11-07 20:16:09 +0700

    test

--- game_app/lib/screen_singler_play/screen_singler_play.dart
+++ game_app/lib/screen_singler_play/screen_singler_play.dart
@@ -18,7 +18,7 @@ class _ScreenSinglePlayState extends State<ScreenSinglePlay> {
       body: SafeArea(
         child: Container(
           child: Column(
-            children: [MyHeader(), MyContentSinglePlay(), MyFooter()],
+            children: [MyHeader(), MyFooter()],
           ),
         ),
       ),
