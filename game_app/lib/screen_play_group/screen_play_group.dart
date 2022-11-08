607d10b9bd5d9918a50b36af77260715291f62be
Tamne2022002 <0306201074@caothang.edu.vn>
2022-11-08 16:46:32 +0700

    play with tean

---
+++ game_app/lib/screen_play_group/screen_play_group.dart
@@ -0,0 +1,16 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+
+class Screen_Team extends StatefulWidget {
+  const Screen_Team({super.key});
+
+  @override
+  State<Screen_Team> createState() => _MyScreen_Team();
+}
+
+class _MyScreen_Team extends State<Screen_Team> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
