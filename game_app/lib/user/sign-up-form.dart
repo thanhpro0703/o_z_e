99072399e2e13321e7e5a8d2ec1d608793720e05
thanhkhoai01 <0306201035@caothang.edu.vn>
2022-11-23 14:06:39 +0700

    sửa sign-up file

--- game_app/lib/user/sign-up-form.dart
+++ game_app/lib/user/sign-up-form.dart
@@ -22,57 +22,59 @@ class _SignUpState extends State<SignUp> {
 
     return Scaffold(
 
-      body: Container(
+      body: SingleChildScrollView(
+        child: Container(
           decoration: BoxDecoration(
             image: DecorationImage(
-              image: AssetImage('images/background-login.jpg'),
+              image: AssetImage('assets/background-login.jpg'),
               fit: BoxFit.fill,
             ),
           ),
-        padding: EdgeInsets.fromLTRB(0,30,30,0),
-        child: Column(
-          children: [
-            Container(
-              padding: EdgeInsets.fromLTRB(30, 0, 0, 0),
-              child: Column(children: [
-                Icon(Icons.account_circle,size: 100,),
+          padding: EdgeInsets.fromLTRB(0,30,30,0),
+          child: Column(
+            children: [
+              Container(
+                  padding: EdgeInsets.fromLTRB(30, 0, 0, 0),
+                  child: Column(children: [
+                    Icon(Icons.account_circle,size: 100,),
 
-                textField(false, 'Firstname...'),
-                textField(false, 'Lastname...'),
-                textField(false, 'Username...'),
-                textField(true, 'Password...'),
-                textField(true, 'Confirm...'),
-                textField(false, 'Email...'),
-                textField(false, 'Phone...'),
+                    textField(false, 'Firstname...'),
+                    textField(false, 'Lastname...'),
+                    textField(false, 'Username...'),
+                    textField(true, 'Password...'),
+                    textField(true, 'Confirm...'),
+                    textField(false, 'Email...'),
+                    textField(false, 'Phone...'),
 
-                SizedBox(height: 10,),
+                    SizedBox(height: 10,),
 
-                ElevatedButton(onPressed: (){}, child: Text('Create'),
-                  style: ButtonStyle(
-                    padding: MaterialStateProperty.all(EdgeInsets.fromLTRB(35, 0, 35, 0)),
-                    shape: MaterialStateProperty.all(RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0))),
-                  ),
-                ),
-                SizedBox(height: 10,),
-                Text('Sign Up With',style: TextStyle(color: Colors.white,fontSize: 18),),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Icon(Icons.facebook,color: Colors.blue,size: 35,),
-                    SizedBox(width: 5,),
-                    Image(image: NetworkImage('https://cdn-icons-png.flaticon.com/512/300/300221.png'),width: 30,)
-                  ],
-                ),
-              ],)
-            ),
-            Align(
-                alignment: Alignment.bottomLeft,
-                child: Transform.scale(
-                  scaleX: -1,
-                  child: Image.asset('images/pigmessage.png',width: 150,),)
-            ),
+                    ElevatedButton(onPressed: (){}, child: Text('Create'),
+                      style: ButtonStyle(
+                        padding: MaterialStateProperty.all(EdgeInsets.fromLTRB(35, 0, 35, 0)),
+                        shape: MaterialStateProperty.all(RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0))),
+                      ),
+                    ),
+                    SizedBox(height: 10,),
+                    Text('Sign Up With',style: TextStyle(color: Colors.white,fontSize: 18),),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(Icons.facebook,color: Colors.blue,size: 35,),
+                        SizedBox(width: 5,),
+                        Image(image: NetworkImage('https://cdn-icons-png.flaticon.com/512/300/300221.png'),width: 30,)
+                      ],
+                    ),
+                  ],)
+              ),
+              Align(
+                  alignment: Alignment.bottomLeft,
+                  child: Transform.scale(
+                    scaleX: -1,
+                    child: Image.asset('assets/pigmessage.png',width: 150,),)
+              ),
 
-          ],
+            ],
+          ),
         ),
       ),
     );
