05d575e9115bb8a203998cd0354b48ef16eb176e
Tamne2022002 <0306201074@caothang.edu.vn>
2022-12-30 11:51:43 +0700

    Tam Chinh MH Solo

--- game_app/lib/screen_solo/screen_solo.dart
+++ game_app/lib/screen_solo/screen_solo.dart
@@ -1,14 +1,7 @@
 import 'dart:ui';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:game_app/api_services.dart';
-import 'package:game_app/button_game/button_enemy.dart';
-import 'package:game_app/button_game/button_start.dart';
 import 'package:game_app/screen_choose_level/screen_choose_level.dart';
-import 'package:game_app/screen_playgame/screen_playgame.dart';
-import 'package:game_app/screen_room/screen_room.dart';
-import '../button_game/button_avatar.dart';
-import '../button_game/button_game.dart';
+import 'package:game_app/screen_info_enemy/screen_info_enemy.dart';
 import '../screen_maingame/screen_maingame.dart';
 
 class Screen_Solo extends StatefulWidget {
@@ -22,197 +15,242 @@ class _MyScreenSolo extends State<Screen_Solo> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
-      decoration: const BoxDecoration(
-          image: DecorationImage(
-        image: AssetImage("assets/background-home.png"),
-        fit: BoxFit.fill,
-      )),
-      child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-        // Container(margin: EdgeInsets.only(top: 70)),
-        Row(
+      body: Container(
+        decoration: const BoxDecoration(
+            image: DecorationImage(
+          image: AssetImage("assets/background-home.png"),
+          fit: BoxFit.fill,
+        )),
+        child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Column(children: [
-              Padding(padding: EdgeInsets.only(top: 50)),
-              ButtonAvatar(
-                height: 100,
-                width: 100,
-                image: 'assets/account.png', username: FirebaseAuth.instance.currentUser!.email!,
-              ),
-              Padding(padding: EdgeInsets.only(top: 10)),
-              Container(
-                  width: MediaQuery.of(context).size.width / 3.5,
-                  height: MediaQuery.of(context).size.width / 3.5,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(30),
-                      color: Color(0xffff6633CC).withOpacity(0.7)),
-                  margin: EdgeInsets.only(top: 10),
-                  child: Center(
-                    child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(
-                            FirebaseAuth.instance.currentUser!.email!,
-                            style: TextStyle(
-                                color: Colors.cyanAccent.withOpacity(0.8),
-                                fontSize: 12),
-                          ),
-                          Padding(padding: EdgeInsets.only(top: 10)),
-                          FutureBuilder(
-                            future: getHighScoreUserName(FirebaseAuth.instance.currentUser!.email!),
-                            builder: (context,snapshot){
-                              return  Text(
-                                'Height Score: ${snapshot.data}',
-                                style: TextStyle(
-                                    color: Colors.cyanAccent.withOpacity(0.8),
-                                    fontSize: 12),
-                              );
-                            }
-
-                          ),
-                          Padding(padding: EdgeInsets.only(top: 10)),
-                          FutureBuilder(
-                              future: totalScore(FirebaseAuth.instance.currentUser!.email!),
-                              builder: (context,snapshot){
-                                return  Text(
-                                  'Rank: ${snapshot.data}',
-                                  style: TextStyle(
-                                      color: Colors.cyanAccent.withOpacity(0.8),
-                                      fontSize: 12),
-                                );
-                              }
-
-                          ),
-                        ]),
-                  )),
-            ]),
-            Container(
-                margin: EdgeInsets.only(top: 70),
-                child: Text('VS',
-                    style: TextStyle(fontSize: 40, color: Colors.cyanAccent))),
-            Column(children: [
-              Padding(padding: EdgeInsets.only(top: 50)),
-              ButtonEnemy(
-                height: 100,
-                width: 100,
-                img: 'assets/add-user.png',
-              ),
-              Padding(padding: EdgeInsets.only(top: 20)),
-              Container(
-                  width: MediaQuery.of(context).size.width / 3.5,
-                  height: MediaQuery.of(context).size.width / 3.5,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(30),
-                      color: Color(0xffff6633CC).withOpacity(0.7)),
-                  child: Center(
-                    child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(
-                            'Name',
-                            style: TextStyle(
-                                color: Colors.cyanAccent.withOpacity(0.8),
-                                fontSize: 12),
-                          ),
-                          Padding(padding: EdgeInsets.only(top: 10)),
-                          Text(
-                            'Point: 198',
-                            style: TextStyle(
-                                color: Colors.cyanAccent.withOpacity(0.8),
-                                fontSize: 12),
-                          ),
-                          Padding(padding: EdgeInsets.only(top: 10)),
-                          Text(
-                            'Rank: 1000',
-                            style: TextStyle(
-                                color: Colors.cyanAccent.withOpacity(0.8),
-                                fontSize: 12),
-                          ),
-                        ]),
-                  )),
-            ])
-          ],
-        ),
-        Padding(padding: EdgeInsets.only(top: 20)),
-        Container(
-            width: MediaQuery.of(context).size.width / 1.5,
-            height: (MediaQuery.of(context).size.width / 4.5),
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(30),
-                color: Color.fromARGB(255, 187, 133, 222).withOpacity(0.6)),
-            child: Center(
-                child: Column(
+            // Container(margin: EdgeInsets.only(top: 70)),
+            Row(
               mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text(
-                  'Topic',
-                  style: TextStyle(fontSize: 20, color: Colors.cyan),
-                ),
-                Padding(padding: EdgeInsets.only(top: 15)),
-                Text('Grade 9 math',
-                    style: TextStyle(fontSize: 12, color: Colors.cyan))
-              ],
-            ))),
-        StartButton(
-            text: "Start",
-            page: Navigator.of(context).push(MaterialPageRoute(
-              builder: (context) => ScreenMainGame(),
-            ))),
-
-        // ElevatedButton(
-        //     onPressed: () {},
-        //     style: ElevatedButton.styleFrom(
-        //         primary: Colors.transparent, shadowColor: Colors.transparent),
-        //     child: Container(
-        //         margin: EdgeInsets.only(top: 50),
-        //         width: MediaQuery.of(context).size.width / 2.5,
-        //         height: MediaQuery.of(context).size.width / 7.5,
-        //         decoration: BoxDecoration(
-        //             borderRadius: BorderRadius.circular(50),
-        //             gradient: const LinearGradient(
-        //               colors: [
-        //                 Color.fromARGB(255, 6, 171, 53),
-        //                 Color.fromARGB(255, 224, 193, 193)
-        //               ],
-        //               begin: Alignment.centerLeft,
-        //               end: Alignment(2, 0.5),
-        //             )),
-        //         child: Center(
-        //           child: TextButton(
-        //             onPressed: () {
-        //               // Navigator.of(context).push(MaterialPageRoute(
-        //               //   builder: (context) => ScreenPlayGame(),
-        //               // ));
-        //             },
-        //             child: Text('Start',
-        //                 style: TextStyle(
-        //                   color: Colors.yellowAccent,
-        //                   fontSize: 20,
-        //                 )),
-        //           ),
-        //         ))),
-
-        Row(mainAxisAlignment: MainAxisAlignment.start, children: [
-          Container(
-              height: 60,
-              child: ButtonWidget(
-                  button: ElevatedButton(
-                      onPressed: () {
-                        Navigator.pop(context);
-                      },
+                Column(children: [
+                  Padding(padding: EdgeInsets.only(top: 50)),
+                  ElevatedButton(
                       style: ElevatedButton.styleFrom(
                           primary: Colors.transparent,
                           shadowColor: Colors.transparent),
+                      onPressed: () {},
                       child: Container(
-                        width: MediaQuery.of(context).size.width / 7,
-                        height: MediaQuery.of(context).size.width / 7,
-                        child: Image(
-                            image: AssetImage('assets/reply-message.png')),
-                      ))))
-        ])
-      ]),
-    ));
+                        width: MediaQuery.of(context).size.width / 5,
+                        height: MediaQuery.of(context).size.width / 5,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          gradient: LinearGradient(
+                            begin: Alignment.topLeft,
+                            end: Alignment(0.7, 1.5),
+                            colors: <Color>[
+                              Color(0xffff8e0e00),
+                              Color(0xfff1f1c18),
+                            ],
+                          ),
+                          boxShadow: [
+                            BoxShadow(
+                                color: Colors.black26,
+                                offset: Offset(0, 4),
+                                blurRadius: 5.0)
+                          ],
+                        ),
+                        child: Center(
+                            child: Image.asset(
+                          'assets/account.png',
+                          width: 45,
+                          height: 50,
+                        )),
+                      )),
+                  Padding(padding: EdgeInsets.only(top: 10)),
+                  Container(
+                      width: MediaQuery.of(context).size.width / 3.5,
+                      height: MediaQuery.of(context).size.width / 3.5,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(30),
+                          color: Color(0xffff6633CC).withOpacity(0.7)),
+                      margin: EdgeInsets.only(top: 10),
+                      child: Center(
+                        child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                'Name',
+                                style: TextStyle(
+                                    color: Colors.cyanAccent.withOpacity(0.8),
+                                    fontSize: 12),
+                              ),
+                              Padding(padding: EdgeInsets.only(top: 10)),
+                              Text(
+                                'Point: 198',
+                                style: TextStyle(
+                                    color: Colors.cyanAccent.withOpacity(0.8),
+                                    fontSize: 12),
+                              ),
+                              Padding(padding: EdgeInsets.only(top: 10)),
+                              Text(
+                                'Rank: 1000',
+                                style: TextStyle(
+                                    color: Colors.cyanAccent.withOpacity(0.8),
+                                    fontSize: 12),
+                              ),
+                            ]),
+                      )),
+                ]),
+                Container(
+                    margin: EdgeInsets.only(top: 70),
+                    child: Text('VS',
+                        style:
+                            TextStyle(fontSize: 40, color: Colors.cyanAccent))),
+                Column(children: [
+                  Padding(padding: EdgeInsets.only(top: 60)),
+                  ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          primary: Colors.transparent,
+                          shadowColor: Colors.transparent),
+                      onPressed: () {
+                        Navigator.of(context).push(MaterialPageRoute(
+                          builder: (context) => ScreenInfoEnemy(),
+                        ));
+                      },
+                      child: Container(
+                        width: MediaQuery.of(context).size.width / 6,
+                        height: MediaQuery.of(context).size.width / 6,
+                        child: Image(image: AssetImage('assets/add-user.png')),
+                      )),
+                  Padding(padding: EdgeInsets.only(top: 25)),
+                  Container(
+                      width: MediaQuery.of(context).size.width / 3.5,
+                      height: MediaQuery.of(context).size.width / 3.5,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(30),
+                          color: Color(0xffff6633CC).withOpacity(0.7)),
+                      child: Center(
+                        child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                'Name',
+                                style: TextStyle(
+                                    color: Colors.cyanAccent.withOpacity(0.8),
+                                    fontSize: 12),
+                              ),
+                              Padding(padding: EdgeInsets.only(top: 10)),
+                              Text(
+                                'Point: 198',
+                                style: TextStyle(
+                                    color: Colors.cyanAccent.withOpacity(0.8),
+                                    fontSize: 12),
+                              ),
+                              Padding(padding: EdgeInsets.only(top: 10)),
+                              Text(
+                                'Rank: 1000',
+                                style: TextStyle(
+                                    color: Colors.cyanAccent.withOpacity(0.8),
+                                    fontSize: 12),
+                              ),
+                            ]),
+                      )),
+                ])
+              ],
+            ),
+            Padding(padding: EdgeInsets.only(top: 20)),
+            Container(
+                width: MediaQuery.of(context).size.width / 1.5,
+                height: (MediaQuery.of(context).size.width / 4.5),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30),
+                    color: Color.fromARGB(255, 187, 133, 222).withOpacity(0.6)),
+                child: Center(
+                    child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Topic',
+                      style: TextStyle(fontSize: 20, color: Colors.cyan),
+                    ),
+                    Padding(padding: EdgeInsets.only(top: 15)),
+                    Text('Grade 9 math',
+                        style: TextStyle(fontSize: 12, color: Colors.cyan))
+                  ],
+                ))),
+            Padding(
+              padding: EdgeInsets.only(top: 30),
+            ),
+            ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                    primary: Colors.transparent,
+                    shadowColor: Colors.transparent),
+                child: Container(
+                    width: MediaQuery.of(context).size.width / 2.5,
+                    height: MediaQuery.of(context).size.width / 7.5,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(30),
+                        gradient: const LinearGradient(
+                          colors: [
+                            Color.fromARGB(255, 6, 171, 53),
+                            Color.fromARGB(255, 224, 193, 193)
+                          ],
+                          begin: Alignment.centerLeft,
+                          end: Alignment(2, 0.5),
+                        )),
+                    child: Center(
+                      child: Text(
+                        'Start',
+                        style: TextStyle(
+                          color: Colors.yellowAccent,
+                          fontSize: 20,
+                        ),
+                      ),
+                    ))),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(top: 20),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  style: ElevatedButton.styleFrom(
+                      primary: Colors.transparent,
+                      shadowColor: Colors.transparent),
+                  child: Container(
+                    width: MediaQuery.of(context).size.width / 4,
+                    height: MediaQuery.of(context).size.width / 7,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      gradient: LinearGradient(
+                        begin: Alignment.topLeft,
+                        end: Alignment(0.7, 1.5),
+                        colors: <Color>[
+                          Color(0xffff8e0e00),
+                          Color(0xfff1f1c18),
+                        ],
+                      ),
+                      boxShadow: [
+                        BoxShadow(
+                            color: Colors.black26,
+                            offset: Offset(0, 4),
+                            blurRadius: 5.0)
+                      ],
+                    ),
+                    child: Image.asset(
+                      'assets/reply-message.png',
+                      width: 10,
+                      height: 10,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
